59afb620ef673f7838ee3452e0ee41ec7f49541d
Erik Grahn <erikgrahn13@gmail.com>
2024-09-16 13:32:42 +0200

    add swiftui application window

---
+++ macos2/mainMacos/mainMacos/mainMacosApp.swift
@@ -0,0 +1,16 @@
+//
+//  mainMacosApp.swift
+//  mainMacos
+//
+//  Created by Erik Grahn on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct mainMacosApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
