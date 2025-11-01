fe62db722462d746ea8f5cb9b5bf23a72a8dba85
Erik Grahn <erikgrahn13@gmail.com>
2025-11-01 12:19:08 +0100

    enable webview for standalone on linux

--- core/gui/platform/linux/singularity_Webview_Linux.cpp
+++ core/gui/platform/linux/singularity_Webview_Linux.cpp
@@ -1,133 +1,51 @@
-#include "webview_linux.h"
+#include "singularity_Webview_Linux.h"
 
-#ifdef __linux__
-#include "../../resource_manager.h"
-#include <gtk/gtk.h>
 #include <iostream>
 #include <string>
-#include <webkit2/webkit2.h>
 
-// Custom URI scheme handler for serving embedded resources
-static void app_scheme_handler(WebKitURISchemeRequest *request, gpointer user_data)
+std::unique_ptr<ISingularityGUI> ISingularityGUI::createView(void *windowHandle, std::function<void()> onReady)
 {
-    const gchar *uri = webkit_uri_scheme_request_get_uri(request);
-    std::string uriStr(uri);
-
-    // Extract path from URI (remove "app://localhost")
-    std::string path = "/";
-    size_t pathStart = uriStr.find("app://localhost");
-    if (pathStart != std::string::npos)
-    {
-        pathStart += 15; // Length of "app://localhost"
-        if (pathStart < uriStr.length())
-        {
-            path = uriStr.substr(pathStart);
-        }
-    }
-
-    // Handle root path
-    if (path.empty() || path == "/")
-    {
-        path = "/index.html";
-    }
-
-    // Get resource from embedded data
-    auto resource = ResourceManager::getResource(path);
-    if (resource)
-    {
-        // Create input stream from memory
-        GInputStream *stream = g_memory_input_stream_new_from_data(resource->data, resource->size, nullptr);
-
-        // Create response
-        webkit_uri_scheme_request_finish(request, stream, resource->size, resource->mimeType.c_str());
-
-        g_object_unref(stream);
-    }
-    else
-    {
-        // Resource not found
-        GError *error = g_error_new(G_IO_ERROR, G_IO_ERROR_NOT_FOUND, "Resource not found: %s", path.c_str());
-        webkit_uri_scheme_request_finish_error(request, error);
-        g_error_free(error);
-    }
+    return std::make_unique<WebViewLinux>(windowHandle, onReady);
 }
 
-WebViewLinux::WebViewLinux() : m_window(nullptr), m_webView(nullptr)
+WebViewLinux::WebViewLinux(void *windowHandle, std::function<void()> onReady)
+    : m_window(static_cast<GtkWidget *>(windowHandle))
 {
+    m_viewReadyCallback = onReady;
 }
 
 WebViewLinux::~WebViewLinux()
 {
-    close();
 }
 
 void WebViewLinux::create(int width, int height, const std::string &title)
 {
-    // Initialize GTK
-    gtk_init(nullptr, nullptr);
-
-    // Create window
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), title.c_str());
-    gtk_window_set_default_size(GTK_WINDOW(window), width, height);
-
-    // Connect close signal
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), nullptr);
-
-    // Register custom URI scheme for embedded resources
-    WebKitWebContext *context = webkit_web_context_get_default();
-    webkit_web_context_register_uri_scheme(context, "app", app_scheme_handler, nullptr, nullptr);
-
-    // Create WebView
-    GtkWidget *webView = webkit_web_view_new_with_context(context);
-    gtk_container_add(GTK_CONTAINER(window), webView);
-
-    m_window = window;
-    m_webView = webView;
-
-    gtk_widget_show_all(window);
 }
 
 void WebViewLinux::navigate(const std::string &url)
 {
-    if (!m_webView)
-        return;
     webkit_web_view_load_uri(WEBKIT_WEB_VIEW(m_webView), url.c_str());
 }
 
 void WebViewLinux::run()
 {
-    gtk_main();
 }
 
 void WebViewLinux::close()
 {
-    if (m_window)
-    {
-        gtk_widget_destroy(GTK_WIDGET(m_window));
-        m_window = nullptr;
-        m_webView = nullptr;
-    }
 }
 
-#else
-// Stub implementation for non-Linux platforms
-WebViewLinux::WebViewLinux() : m_window(nullptr), m_webView(nullptr)
+void WebViewLinux::resize(int width, int height)
 {
 }
-WebViewLinux::~WebViewLinux()
+
+void WebViewLinux::executeScript(const std::string &script)
 {
 }
-void WebViewLinux::create(int width, int height, const std::string &title)
+
+void WebViewLinux::initialize()
 {
+    m_webView = webkit_web_view_new();
+    gtk_window_set_child(GTK_WINDOW(m_window), m_webView);
+    OnWebViewReady();
 }
-void WebViewLinux::navigate(const std::string &url)
-{
-}
-void WebViewLinux::run()
-{
-}
-void WebViewLinux::close()
-{
-}
-#endif
--- core/gui/platform/linux/singularity_Webview_Linux.h
+++ core/gui/platform/linux/singularity_Webview_Linux.h
@@ -1,19 +1,27 @@
 #pragma once
 
+#include "../../singularity_Webview.h"
+#include <gtk/gtk.h>
+#include <memory>
 #include <string>
+#include <webkit/webkit.h>
 
-class WebViewLinux
+class WebViewLinux : public ISingularityGUI
 {
   public:
-    WebViewLinux();
+    WebViewLinux(void *windowHandle = nullptr, std::function<void()> onReady = nullptr);
     ~WebViewLinux();
 
-    void create(int width, int height, const std::string &title);
-    void navigate(const std::string &url);
-    void run();
-    void close();
+    void create(int width, int height, const std::string &title) override;
+    void createAsChild(void *parentWindow, int width, int height) {};
+    void navigate(const std::string &url) override;
+    void run() override;
+    void close() override;
+    void resize(int width, int height) override;
+    void executeScript(const std::string &script);
+    void initialize() override;
 
   private:
-    void *m_window;  // GtkWidget*
-    void *m_webView; // WebKitWebView*
+    GtkWidget *m_window;
+    GtkWidget *m_webView;
 };
