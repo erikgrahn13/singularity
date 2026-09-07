97ddfbff30da06a5c446069493c20bd2e15ceadd
Erik Grahn <erikgrahn13@gmail.com>
2026-09-07 17:56:45 +0200

    detele more obsolete files

--- cmake/CHOC.cmake
+++
@@ -1,7 +0,0 @@
-FetchContent_Declare(
-    choc
-    GIT_REPOSITORY https://github.com/Tracktion/choc.git
-    GIT_TAG main
-    GIT_SHALLOW TRUE
-)
-FetchContent_MakeAvailable(choc)
--- cmake/QUICKJS.cmake
+++
@@ -1,8 +0,0 @@
-FetchContent_Declare(
-    quickjs
-    URL https://github.com/quickjs-ng/quickjs/archive/refs/tags/v0.12.1.tar.gz
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    EXCLUDE_FROM_ALL
-)
-
-FetchContent_MakeAvailable(quickjs)
